1451166a81b2710a24723c2450d2538c7e54580f
Tyson Do <doraeminemon@gmail.com>
2024-05-01 18:52:12 +0700

    Add basic AidropJobStore class

--- src/models/airdropJob.ts
+++ src/models/airdropJob.ts
@@ -1,3 +1,27 @@
+import { nanoid } from "nanoid";
+
 export class AirdropJobStore {
-  // TODO: Implement the in-memory store logic
+  jobDetails: Record<
+    string,
+    { nftContractAddress: string; redeemed: number; quantity: number }
+  > = {};
+  constructor() {}
+  generateAirdropJobDetails(contractAddress: string, quantity: number = 10) {
+    this.jobDetails[nanoid()] = {
+      nftContractAddress: contractAddress,
+      redeemed: 0,
+      quantity,
+    };
+  }
+  retrieveAirdropDetails(redeemCode: string) {
+    return this.jobDetails[redeemCode];
+  }
+  markAirdropRedeemed(redeemCode: string) {
+    const job = this.jobDetails[redeemCode];
+    if (job.redeemed < job.quantity) {
+      this.jobDetails[redeemCode].redeemed += 1;
+      return;
+    }
+    throw new Error("Limit exceeded");
+  }
 }
